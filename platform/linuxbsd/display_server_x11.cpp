acfbedd58e1d2761eef326299fdbbf0a601ec7d1
jordi <creptthrust@gmail.com>
2022-02-03 20:35:46 -0600

    2nd Attempt to fix X11 Issues

--- platform/linuxbsd/display_server_x11.cpp
+++ platform/linuxbsd/display_server_x11.cpp
@@ -1064,11 +1064,46 @@ float DisplayServerX11::screen_get_refresh_rate(int p_screen) const {
 
 	//Use xrandr to get screen refresh rate.
 	if (xrandr_ext_ok) {
-		XRRScreenConfiguration *config = XRRGetScreenInfo(x11_display, RootWindow(x11_display, 0));
-		if (config) {
-			int* r;
-			print_line(XRRConfigRates(config, p_screen, r));
-			return 0.0;
+		XRRScreenResources *screen_info = XRRGetScreenResources(x11_display, windows[MAIN_WINDOW_ID].x11_window);
+		if (screen_info) {
+			RRMode current_mode = 0;
+			xrr_monitor_info *monitors = nullptr;
+
+			if (xrr_get_monitors) {
+				int count = 0;
+				monitors = xrr_get_monitors(x11_display, windows[MAIN_WINDOW_ID].x11_window, true, &count);
+				ERR_FAIL_INDEX_V(p_screen, count, SCREEN_REFRESH_RATE_FALLBACK);
+			}
+			else {
+				ERR_PRINT("An error occured while trying to get the screen refresh rate.");
+				return SCREEN_REFRESH_RATE_FALLBACK;
+			}
+
+			bool found_active_mode = false;
+			for (int crtc = 0; crtc < screen_info.ncrtc;crtc++) { // Loop through outputs to find which one is currently outputting.
+				XRRCrtInfo *monitor_info = XRRGetCrtcInfo(x11_display, screen_info, screen_info->crtcs[crtc]);
+				if (monitor_info->x != monitors[p_screen].x || monitor_info->y != monitors[p_screen].y) { // If X and Y aren't the same as the monitor we're looking for, this isn't the right monitor. Continue.
+					continue;
+				}
+
+				if (monitor_info->mode != None) {
+					current_mode = monitor_info->mode;
+					found_active_mode = true;
+					break;
+				}
+			}
+
+			if (found_active_mode) {
+				for (int mode = 0; mode < screen_info.nmodes; mode++) {
+					XRRModeInfo m_info = screen_info->modes[mode];
+					if (m_info.id == current_mode) {
+						return (float) m_info.dotClock / ((float)m_info.hTotal * (float)m_info.vTotal);
+					}
+				}
+			}
+
+			ERR_PRINT("An error occured while trying to get the screen refresh rate."); // We should have returned the refresh rate by now. An error must have occured.
+			return SCREEN_REFRESH_RATE_FALLBACK;
 		} else {
 			ERR_PRINT("An error occured while trying to get the screen refresh rate.");
 			return SCREEN_REFRESH_RATE_FALLBACK;
